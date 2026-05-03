5b748ff86e1effe552be4b460e398e9c92235b8d
Hrishi2520 <hrishikeshshedge0@gmail.com>
2026-05-03 21:48:52 +0530

    Open API Defination Project INFO

--- account/src/main/java/com/microservice/account/AccountApplication.java
+++ account/src/main/java/com/microservice/account/AccountApplication.java
@@ -1,11 +1,29 @@
 package com.microservice.account;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+/*@ComponentScan({@ComponentScan("com.microservice.external.controller")})
+@EnableJpaRepositories("com.microservice.external.repository")
+@EntityScan("com.microservice.external.entities")*/
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice Rest Api Documentation",
+				description = "Bank Accounts Microservices REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Hrishikesh",
+						email = "hrishikesh@gmail.com"
+				)
+
+		)
+)
 public class AccountApplication {
 
 	public static void main(String[] args) {
