5bfe837b844763362a7248d9e8714a4abd3eb9bc
Hrishi2520 <hrishikeshshedge0@gmail.com>
2026-05-03 19:20:38 +0530

    Validation

--- account/src/main/java/com/microservice/account/entity/BaseEntity.java
+++ account/src/main/java/com/microservice/account/entity/BaseEntity.java
@@ -4,6 +4,10 @@ import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
@@ -11,14 +15,20 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 public class BaseEntity {
+
+    @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdAt;
 
+    @CreatedBy
     @Column(updatable = false)
     private String createdBy;
 
+    @LastModifiedDate
     @Column(insertable = false)
     private LocalDateTime updatedAt;
+
+    @LastModifiedBy
     @Column(insertable = false)
     private String updatedBy;
 
